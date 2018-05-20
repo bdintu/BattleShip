6080ea88e9412caaed9fa6f23f663059f68cc830
j <j@j.j>
2018-05-20 14:49:47 +0700

    test k1, k2

--- BattleShip.vhd
+++ BattleShip.vhd
@@ -4,13 +4,14 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity BattleShip is
 	Port(	A, B	: in  STD_LOGIC_VECTOR (2 downto 0);
-			C		: out  STD_LOGIC_VECTOR (6 downto 0)
+			K1, K2	: out  STD_LOGIC_VECTOR (7 downto 0)
 	);
 end BattleShip;
 
 architecture Behavioral of BattleShip is
 begin
 
-	C <= "1100111";
+	K1 <= "1111111";
+	K2 <= "1111111";
 
 end Behavioral;
